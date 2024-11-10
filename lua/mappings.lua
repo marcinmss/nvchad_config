ac2b0fd3be679416c4f8291a45846e83781aac9a
marcinmss <marciomsantoss@gmail.com>
2024-11-10 11:35:01 -0300

    Deleted useless mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,8 +4,6 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
--- map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 map("n", "<leader>u", "<cmd> UndotreeToggle <cr>")
 
 -- better search terms
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,6 @@ return {
   },
   {
     "rcarriga/nvim-dap-ui",
-    -- dependencies = "mfussenegger/nvim-dap",
     dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     config = function()
       local dap = require "dap"
